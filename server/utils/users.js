491a6c861559f21b31fc2b8ffaffbe0e8ca76b44
lmislm <wszdlm@gmail.com>
2018-03-06 15:23:56 +0800

    store users

--- server/utils/users.js
+++ server/utils/users.js
@@ -21,12 +21,22 @@ class Users {
     }
     removeUser(id) {
         //return user that was remove
+        var user = this.getUser(id);
+
+        if(user) {
+            this.users = this.users.filter((user) => user.id !== id);
+        }
+
+        return user;
     }
     getUser (id) {
-
+        return this.users.filter((user) => user.id === id)[0]
     }
     getUserList(room){
-        
+        var users = this.users.filter((user) => user.room === room);
+        var namesArray = users.map((user) => user.name);
+
+       return namesArray; 
     }
 }
 
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -3,21 +3,22 @@ const expect = require('expect');
 const {Users} = require('./users');
 
 describe('Users', () => {
+    var users;
 
     beforeEach(() => {
         users = new Users();
         users.users = [{
             id: '1',
-            name: '王一样',
-            room: '爸爸屋1'
+            name: '赵五',
+            room: '赵五房'
         },{
             id: '2',
-            name: '张二',
-            room: '爸爸屋2'
+            name: '张三',
+            room: '张三房'
         },{
             id: '3',
-            name: '李三',
-            room: '爸爸屋3'
+            name: '李四',
+            room: '张三房'
         }]
     })
 
@@ -32,4 +33,48 @@ describe('Users', () => {
 
         expect(users.users).toEqual([user]);
     })
+    
+    
+    it('应移除用户', () => {
+        var userId = '1';
+        var user = users.removeUser(userId);
+
+        expect(user.id).toBe(userId);
+        expect(users.users.length).toBe(2);
+    })
+    it('不应移除用户', () => {
+       var userId = "99";
+       var user = users.removeUser(userId);
+
+       expect(user).toNotExist();
+       expect(users.users.length).toBe(3);
+    })
+
+    it('应该找到用户', () => {
+        var userId = '2';
+        var user = users.getUser(userId);
+
+        expect(user.id).toBe(userId);
+    })
+
+    it('不应该找到用户', () => {
+        var userId = '89';
+        var user = users.getUser(userId);
+
+        expect(user).toNotExist();
+    })
+
+    it('应该返回张三房间的用户名', () => {
+        var userList = users.getUserList('张三房');
+
+        expect(userList).toEqual(['张三','李四'])
+    })
+
+    it('应该返回赵五房间的用户名', () => {
+        var userList = users.getUserList('赵五房');
+
+        expect(userList).toEqual(['赵五'])
+    })
+
+
 })
